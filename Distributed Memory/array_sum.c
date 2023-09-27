7a4579b74cb229a55ee8eb412a9d5d15b41ebcef
Aniket Gupta <aniketgupta3001@gmail.com>
2023-09-27 12:14:11 +0530

    Update array_sum.c

--- Distributed Memory/array_sum.c
+++ Distributed Memory/array_sum.c
@@ -1,31 +1,42 @@
+// compile using mpicc file_name.c
+// run using mpirun -np 4 ./a.out
+
 #include <mpi.h>
 #include <stdio.h>
 
 #define SIZE 16 // Replace with your desired array size
 
 int main(int argc, char **argv) {
-    int rank = 0, nproc = 4;
+    int rank, nprocs;
 
     MPI_Init(&argc, &argv);
 
     // 1. Get to know your world
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &nproc);
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
 
-    int array[SIZE]; // Initialize and assume (SIZE % nproc = 0)
+    int array[SIZE]; // Initialize and assume (SIZE % nprocs = 0)
+
+    // Initialize the array with sequential values
+    for (int i = 0; i < SIZE; i++) {
+        array[i] = i;
+    }
 
     // 2. Calculate local sum
-    int my_sum = 0, chunk = SIZE / nproc;
-    for (int i = rank * chunk; i < (chunk + 1) * rank; i++) {
+    int my_sum = 0, chunk = SIZE / nprocs;
+
+    for (int i = rank * chunk; i < (rank + 1) * chunk; i++) {
         my_sum += array[i];
     }
 
     // 3. All non-root processes send results to the root process (rank=0)
     if (rank > 0) {
         MPI_Send(&my_sum, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-    } else { // Executed only at rank=0
+    } 
+    
+    else { // Executed only at rank = 0
         int total_sum = my_sum, tmp;
-        for (int src = 1; src < nproc; src++) {
+        for (int src = 1; src < nprocs; src++) {
             MPI_Status status;
             MPI_Recv(&tmp, 1, MPI_INT, src, 0, MPI_COMM_WORLD, &status);
             total_sum += tmp;
